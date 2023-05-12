800525728b354c9309d144cb874fdc05b85aeec3
TurtleDAO <79334084+TURTLdao@users.noreply.github.com>
2023-05-12 14:10:43 +0100

    Update hero btns adding discord

--- src/modules/Sections/Hero.js
+++ src/modules/Sections/Hero.js
@@ -37,11 +37,19 @@ const Hero = () => (
           <div>
             <Link
               className="circleBtn twitter"
+              href="https://discord.gg/6KwXqWwPgC"
+              target="_blank"
+            >
+              {' '}
+              <img src='https://static.cdnlogo.com/logos/d/64/discord.png' />{' '}
+            </Link>
+            <Link
+              className="circleBtn twitter"
               href="https://twitter.com/Froggieo_"
               target="_blank"
             >
               {' '}
-              <Twitter />{' '}
+              <img src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c53e.png' />{' '}
             </Link>
           </div>
         </div>

de32c92505f7365112f548b12e41216d9163e85a
TurtleDAO <79334084+TURTLdao@users.noreply.github.com>
2023-05-12 14:11:10 +0100

    Update footer btns adding Discord

--- src/modules/footer/Footer.js
+++ src/modules/footer/Footer.js
@@ -15,7 +15,12 @@ const Footer = () => (
       <Link href="https://cardanoscan.io/token/79906b9c8d2fbddeba9658387a2a1187f3edd8f546e5dc49225710a146524f47474945" target="_blank"> Cardano SCAN </Link>
     </div>
     <div>
-      <Link className="circleBtn twitter" href="https://twitter.com/Froggieo_" target="_blank"> <Twitter/> </Link>
+      <Link className="circleBtn twitter" href="https://discord.gg/6KwXqWwPgC" target="_blank" >
+        <img src='https://static.cdnlogo.com/logos/d/64/discord.png' />
+      </Link>
+      <Link className="circleBtn twitter" href="https://twitter.com/Froggieo_" target="_blank">
+        <img src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c53e.png' />
+      </Link>
     </div>
   </div>
   <div className="copyright">

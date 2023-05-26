175af02719afc9f892df2103bd398e3b737e4b16
TurtleDAO <79334084+TURTLdao@users.noreply.github.com>
2023-05-26 15:51:23 +0100

    Update Footer.js

--- src/modules/footer/Footer.js
+++ src/modules/footer/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => (
   <h2 className='strokeme'>JOIN THE POND</h2>
   <div className='HeroBtns'>
     <div>
-      <Link href="https://app.minswap.org/swap?currencySymbolA=&tokenNameA=&currencySymbolB=79906b9c8d2fbddeba9658387a2a1187f3edd8f546e5dc49225710a1&tokenNameB=46524f47474945" target="_blank"> BUY NOW VIA MINISWAP</Link>
+      <Link href="https://app.minswap.org/swap?currencySymbolA=&tokenNameA=&currencySymbolB=79906b9c8d2fbddeba9658387a2a1187f3edd8f546e5dc49225710a1&tokenNameB=46524f47474945" target="_blank"> BUY NOW VIA MINSWAP</Link>
       <Link href="https://cardanoscan.io/token/79906b9c8d2fbddeba9658387a2a1187f3edd8f546e5dc49225710a146524f47474945" target="_blank"> Cardano SCAN </Link>
     </div>
     <div>

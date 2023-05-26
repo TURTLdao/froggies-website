1542554cee48df8f91566e9eede39b7485c952a9
TurtleDAO <79334084+TURTLdao@users.noreply.github.com>
2023-05-26 15:48:48 +0100

    Rename miniswap to minswap

--- src/modules/Sections/HowToBuy.js
+++ src/modules/Sections/HowToBuy.js
@@ -18,8 +18,8 @@ const HowToBuy = () => (
       <p>If you don't have any Cardano in your wallet, make sure you buy some to cover what you want to buy and fee's. Cardano, $ADA, can be bought of most exchanges.</p>
     </div>
     <div className="HTB-Block">
-      <h3 className='strokeme'>3. Go to Miniswap </h3>
-      <p>Connect to Miniswap. Go to app.miniswap.org in your the browser or  inside your Eternl app. Connect your wallet. Paste the $FROGGIE token address into Miniswap, select <b>FROGGIE</b>, and confirm. When Eternl prompts you for a wallet signature, sign.</p>
+      <h3 className='strokeme'>3. Go to Minswap </h3>
+      <p>Connect to Minswap. Go to app.minswap.org in your the browser or  inside your Eternl app. Connect your wallet. Paste the $FROGGIE token address into Minswap, select <b>FROGGIE</b>, and confirm. When Eternl prompts you for a wallet signature, sign.</p>
     </div>
   </div>
   </div>

961dab30529dee2a4cff4a754c1f164abb3da667
TurtleDAO <79334084+TURTLdao@users.noreply.github.com>
2023-05-26 15:49:13 +0100

    Update Hero.js

--- src/modules/Sections/Hero.js
+++ src/modules/Sections/Hero.js
@@ -24,7 +24,7 @@ const Hero = () => (
               
             >
               {' '}
-              BUY NOW VIA MINISWAP{' '}
+              BUY NOW VIA MINSWAP{' '}
             </Link>
             <Link
               href="https://cardanoscan.io/token/79906b9c8d2fbddeba9658387a2a1187f3edd8f546e5dc49225710a146524f47474945"
